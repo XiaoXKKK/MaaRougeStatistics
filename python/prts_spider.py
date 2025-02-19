7e2fc5a40ad3d6754e14489bf359b95a9253f740
XiaoXKKK <875440114@qq.com>
2025-02-19 16:26:59 +0800

    fix: transparent background

--- python/prts_spider.py
+++ python/prts_spider.py
@@ -1,19 +1,21 @@
 import scrapy
-from scrapy.pipelines.images import ImagesPipeline
+from scrapy.pipelines.files import FilesPipeline
 from scrapy.utils.project import get_project_settings
 
 class WikiImageItem(scrapy.Item):
-    image_urls = scrapy.Field()
-    images = scrapy.Field()
+    file_urls = scrapy.Field()
+    files = scrapy.Field()
 
 class PRTSWikiSpider(scrapy.Spider):
     name = 'prts_wiki'
     allowed_domains = ['prts.wiki', 'torappu.prts.wiki']
     start_urls = ['https://prts.wiki/w/萨卡兹的无终奇语/想象实体图鉴']
+
     def parse(self, response):
         # 提取所有包含收藏品图片的img标签
         img_selectors = response.xpath('//tr/td[contains(@style, "background:#464646")]//img')
-        image_urls = []
+
+        file_urls = []
         for img in img_selectors:
             # 优先使用data-src，若不存在则使用src
             url = img.xpath('@data-src').get() or img.xpath('@src').get()
@@ -23,29 +25,33 @@ class PRTSWikiSpider(scrapy.Spider):
                 # 将协议头转换为https
                 if full_url.startswith('//'):
                     full_url = f'https:{full_url}'
-                image_urls.append(full_url)
+                file_urls.append(full_url)
+
         # 去重处理
-        image_urls = list(set(image_urls))
+        file_urls = list(set(file_urls))
+        
         item = WikiImageItem()
-        item['image_urls'] = image_urls
+        item['file_urls'] = file_urls
         yield item
 
-class CustomImagesPipeline(ImagesPipeline):
+class CustomFilesPipeline(FilesPipeline):
     def file_path(self, request, response=None, info=None, *, item=None):
-        # 使用原始文件名保存图片
-        image_guid = request.url.split('/')[-1]
-        return image_guid
-
+        # 从URL中提取文件名
+        file_name = request.url.split('/')[-1]
+        return file_name
+    
 if __name__ == '__main__':
     from scrapy.crawler import CrawlerProcess
+
     
     # 项目设置
     settings = get_project_settings()
     settings.update({
-        'ITEM_PIPELINES': {'__main__.CustomImagesPipeline': 1},
-        'IMAGES_STORE': './relics',  # 图片保存路径
+        'ITEM_PIPELINES': {'__main__.CustomFilesPipeline': 1},
+        'FILES_STORE': 'assets/resource/image/relics',  # 图片保存路径
         'DOWNLOAD_DELAY': 0.5,       # 下载延迟
-        'AUTOTHROTTLE_ENABLED': True # 启用自动限速
+        'AUTOTHROTTLE_ENABLED': True, # 启用自动限速
+        'MEDIA_ALLOW_REDIRECTS': True  # 允许媒体重定向
     })
 
     process = CrawlerProcess(settings)
--- python/relic_counter.py
+++ python/relic_counter.py
@@ -10,7 +10,22 @@ from maa.custom_recognition import CustomRecognition
 from maa.notification_handler import NotificationHandler, NotificationType
 
 import cv2
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
 
+def cv2AddChineseText(img, text, position, textColor, textSize):
+    if (isinstance(img, np.ndarray)):  # 判断是否OpenCV图片类型
+        img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    draw = ImageDraw.Draw(img)
+    fontStyle = ImageFont.truetype(
+        "simsun.ttc", textSize, encoding="utf-8")
+    # 绘制文本
+    draw.text(position, text, textColor, font=fontStyle)
+    # 转换回OpenCV格式
+    return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+
+
+import json
 
 # for register decorator
 resource = Resource()
@@ -83,15 +98,46 @@ class RelicRecognition(CustomAction):
         # context.override_next(argv.node_name, ["TaskA", "TaskB"])
 
         image = context.tasker.controller.cached_image
+        image_copy = image.copy()
         # context.tasker.controller.post_click(100, 100).wait()
 
         relics_path = os.path.join("assets/resource", "image/relics")
+
+        with open('python/roguelike_topic_table.json', "r") as f:
+            relics = json.load(f)['details']['rogue_4']['items']
+
+        relic_list = []
+
         for relic_file in os.listdir(relics_path):
             reco_detail = context.run_recognition(
-                "Relic", image, {"Relic": {"recognition": "FeatureMatch", "template": "relics/"+relic_file}}
+                "Relic", image, {"Relic": {"recognition": "FeatureMatch", 
+                                           "template": "relics/"+relic_file,
+                                           "ordered_by": "Score"},
+                                           "roi": [
+                                                322,
+                                                152,
+                                                725,
+                                                500
+                                            ]}
             )
             if reco_detail is not None:
                 print(reco_detail)
+                w, h = reco_detail.box.w, reco_detail.box.h
+                if w > 90 or h > 90:
+                    continue
+                relic_name = relics[relic_file.split('.')[0]]['name']
+                relic_list.append(relic_name)
+                # draw rectangle
+                cv2.rectangle(image_copy, (reco_detail.box.x, reco_detail.box.y), 
+                              (reco_detail.box.x + w, reco_detail.box.y + h), (0, 255, 0), 2)
+                # add text
+                image_copy = cv2AddChineseText(image_copy, relic_name, (reco_detail.box.x, reco_detail.box.y), (0, 255, 0), 12)
+        
+        # save copy image
+        cv2.imwrite("relics_recognition.jpg", image_copy)
+
+        print(relic_list)
+        print("nums:", len(relic_list))
 
         return CustomAction.RunResult(success=True)
 
